c5167790f97a6fe92f21f55b6aa74e2a78026953
veronica <null.color@qq.com>
2021-11-18 23:43:06 +0800

    轮询录播视频列表

--- src/pages/record/Record.tsx
+++ src/pages/record/Record.tsx
@@ -1,14 +1,31 @@
-import { Input } from 'antd';
+import { Input, Radio, Space } from 'antd';
 import { useState } from 'react';
+import { request } from 'umi'
+import { useRequest } from 'ahooks';
 
 const Record = () => {
-  const [streamURL, setStreamURL] = useState("http://159.75.87.63/record/stream-1-1636793444.mp4")
+  const { data } = useRequest(
+    () => request<string[]>('http://159.75.87.63/filelist'),
+    {
+      pollingInterval: 1000,
+      onError(e) {
+        console.log(e)
+      }
+    }
+  )
+  const [streamURL, setStreamURL] = useState("")
 
   return (
     <>
       <h1>
         视频URL：
-        <Input value={streamURL} onChange={e => setStreamURL(e.target.value)} />
+        <Radio.Group onChange={e => setStreamURL(e.target.value)} value={streamURL}>
+          <Space direction="vertical">
+            { data?.map(file => (
+              <Radio value={`http://159.75.87.63/record/${file}`} key={file}>{ file }</Radio>
+            )) }
+          </Space>
+        </Radio.Group>
       </h1>
       <div style={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <video src={streamURL} controls />
